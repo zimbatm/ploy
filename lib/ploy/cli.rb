b16dd126f2dbe52722ab149bf7e16f564cedea59
Jonas Pfenniger <jonas@pfenniger.name>
2013-02-28 11:41:16 +0000

    Move the vagrant stuff into ploy-scripts

--- lib/ploy/cli.rb
+++ lib/ploy/cli.rb
@@ -6,33 +6,6 @@ module Ploy
   class CLI < Thor
     class ConfigurationError < Ploy::Errors::Error; end
 
-    desc "build", "WIP: Create a build of your code using Vagrant"
-    def build
-      require 'vagrant'
-      require 'vagrant/cli'
-      require 'vagrant/util/platform'
-      #project_root = Dir.pwd
-      env = Vagrant::Environment.new(
-        cwd: File.expand_path('../script', __FILE__),
-        ui_class: $stdout.tty? ? Vagrant::UI::Colored : Vagrant::UI::Basic
-      )
-      env.load!
-      env.cli('up', '--no-provision')
-      # TODO: Setup some custom stuff here
-      #p [:provisioners, env.config.vm.provisioners]
-      env.cli('provision')
-      env.cli('destroy')
-    rescue Vagrant::Errors::VagrantError => e
-      p e
-      exit e.status_code if e.respond_to?(:status_code)
-      exit 999 
-    end
-
-    desc "connect", "Connects to a remote url and configures your repo"
-    def connect
-
-    end
-
     desc "init", "Adds default slugify and install scripts in your project"
     def init
       require 'ploy/app'
