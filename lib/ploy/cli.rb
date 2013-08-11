b5539e90900f41ac894a040e9810f962052b1b22
Vivien Schilis <vivien.schilis@gmail.com>
2013-08-11 23:35:22 +0100

    Added display table helper

--- lib/ploy/cli.rb
+++ lib/ploy/cli.rb
@@ -28,6 +28,41 @@ module Ploy
         raise ConfigurationError, "Unknown token" unless token
         @client = Client.new(host: host, token: token)
       end
+
+      def display_table(objects, columns, headers)
+        lengths = []
+        columns.each_with_index do |column, index|
+          header = headers[index]
+          items = [header].concat(objects.map { |o| o[column].to_s })
+          lengths << items.map { |i| i.to_s.length }.sort.last
+        end
+        lines = lengths.map {|length| "-" * length}
+        lengths[-1] = 0 # remove padding from last column
+        display_row headers, lengths
+        display_row lines, lengths
+        objects.each do |row|
+          display_row columns.map { |column| row[column] }, lengths
+        end
+      end
+
+      def display_row(row, lengths)
+        row_data = []
+        row.zip(lengths).each do |column, length|
+          format = column.is_a?(Fixnum) ? "%#{length}s" : "%-#{length}s"
+          row_data << format % column
+        end
+        display(row_data.join("  "))
+      end
+
+      def display(msg="", new_line=true)
+        if new_line
+          puts(msg)
+        else
+          print(msg)
+          $stdout.flush
+        end
+      end
+
     end
 
     class App < Thor
@@ -70,7 +105,22 @@ module Ploy
 
       desc "list", "Lists the build jobs"
       def list
-        pp client.get_build_list(options[:app_name])
+        builds = client.get_build_list(options[:app_name])
+
+        builds.map! do |build|
+          {
+            'id' => build['id'],
+            'commit_id' => build['commit_id'][0..6], 
+            'branch' => build['branch'],
+            'state' => build['state'],
+          }
+        end
+
+        display_table(
+          builds,
+          %w( id commit_id branch state),
+          ["ID", "Commit", "Branch", "State"]
+        )
       end
       #alias ls list
     end
